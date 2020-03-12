68a38e4c01c518a3a27152e16e580d059b5dbb48
Ericcentrik <enaiman1@gmail.com>
2020-03-11 22:10:42 -0400

    created an array of objects that were passed to the card components so each card is dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Moutin',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'here is a picture of a snowy mountain'
+    },
+    {
+      title: 'Mountain biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking1222',
+      content: 'I did some biking today'
+    }
+  ]
 }
